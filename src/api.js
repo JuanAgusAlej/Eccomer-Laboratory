d7a3e68d7c140bbcbfdc9cf2395f2667d7233f5e
JuanAgusAlej <alejandro4juan@gmail.com>
2025-09-20 09:41:30 -0300

    Se agrego la busqueda por texto (#22)

--- src/api.js
+++ src/api.js
@@ -7,7 +7,9 @@ const fetchData = async () => {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
+    localStorage.setItem("products", JSON.stringify(data));
     console.log(data);
+
     return data;
   } catch (error) {
     console.error("Fetch error:", error);
@@ -30,19 +32,18 @@ const fetchDataDetallado = async (id) => {
   }
 };
 
-// const fetchData = async () => {
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     const data = await response.json();
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.error("Fetch error:", error);
-//     throw error;
-//   }
-// };
+const SearchProduct = async (text) => {
+  try {
+    const products = JSON.parse(localStorage.getItem("products")) || [];
+    const data = products.filter((product) =>
+      product.title.toLowerCase().includes(text.toLowerCase())
+    );
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
 
 export { fetchData, fetchDataDetallado };
